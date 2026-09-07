258aae03a873b80352dfcaca8f4536a79e9cfd0b
divyashinde1509-hash <divyashinde1509@gmail.com>
2026-09-07 19:08:57 +0530

    updated code

--- customer/cart.php
+++ customer/cart.php
@@ -479,3 +479,22 @@ require_once __DIR__ . '/includes/header.php';
     </div>
 
 </div>
+<script>
+
+function updateCartQuantity(cartId, quantity) {
+
+    if (quantity < 1) {
+        quantity = 1;
+    }
+
+    window.location.href =
+        "cart.php?update_id=" +
+        cartId +
+        "&quantity=" +
+        quantity;
+}
+
+</script>
+
+
+<?php require_once __DIR__ . '/includes/footer.php'; ?>
